173db3ffe79eaa139d17653a15e2743de05f5582
unknown <enzoragazzoni21@gmail.com>
2023-10-30 10:00:17 -0300

    feat: StartScreen done, plus customized css

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import StartScreen from './components/StartScreen'
 
 function App() {
 
   return (
     <>
-        <h2>Secret Word</h2>
+      <StartScreen/>
     </>
   )
 }
---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./StartScreen.css"
+
+const StartScreen = () => {
+  return (
+    <div className="StartScreen">
+        <h1>Secret Word</h1>
+        <h3>Clique no botão abaixo para começar a jogar</h3>
+        <button>Começar o jogo</button>
+    </div>
+  )
+}
+
+export default StartScreen
